c8f8e57c362aaafdc980854fe552ab079d4d7d94
vaibhav-bhoir <bhoirvaibhav90@gmail.com>
2021-05-13 19:56:26 +0530

    initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ function About() {
                                     <p>I'm a web developer . I have build several websites , looking at the design I can convert it into a HTML CSS template with pixel perfect precision . I use bootstrap very often , so the turn around of my project is very quick and I always use GIT to track my progress and files, as well to collaborate with other developers also i have knowledge of nodejs, expressjs and database. <br /> I'm looking for the position where I can progress and challenge my skills.</p>
                                     
                                     <div className="mt-3 mb-5">
-                                        <a className='btn btn-primary text-white' target="_blank" href="../images/Vaibhav's Resume.pdf">Download Resume</a>
+                                        <a className='btn btn-primary text-white'  rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/12oLbPyz9DUeocdihhhPYKaUlX-TMUtt0/view?usp=drivesdk">Download Resume</a>
                                     </div>
                                 </div>
 
